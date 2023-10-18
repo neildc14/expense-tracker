dcb972cbd8ac6e8b67f3a3333b90523072c34d2d
neildc14 <genoznightmare@gmail.com>
2023-10-18 21:15:03 +0800

    latest

--- src/pages/expenses/AddExpenses.jsx
+++ src/pages/expenses/AddExpenses.jsx
@@ -1,7 +1,81 @@
-import React from "react";
+import {
+  Box,
+  TextField,
+  Typography,
+  Select,
+  MenuItem,
+  FormControl,
+  InputLabel,
+  Button,
+  Paper,
+} from "@mui/material";
+import React, { useState } from "react";
 
 const AddExpenses = () => {
-  return <div>Add Expenses</div>;
+  const [select, setSelect] = useState("");
+
+  const handleSelect = (e) => {
+    setSelect(e.target.value);
+  };
+  return (
+    <Box>
+      <Typography variant="h6" noWrap>
+        Add Expenses
+      </Typography>
+      <Paper elevation={3} sx={{ padding: "2rem 1rem", marginTop: "1rem" }}>
+        <Box component="form" className="expenses-form">
+          <TextField
+            id="outlined-basic"
+            label="Description"
+            variant="outlined"
+            size="small"
+            gutterBottom
+            fullWidth
+          />
+          <TextField
+            id="outlined-basic"
+            label="Amount"
+            type="number"
+            variant="outlined"
+            size="small"
+            gutterBottom
+            fullWidth
+          />
+          <FormControl>
+            <InputLabel id="categories">Categories</InputLabel>
+            <Select
+              labelId="categories"
+              label="Categories"
+              size="small"
+              fullWidth
+              value={select}
+              onChange={handleSelect}
+            >
+              <MenuItem value="groceries">Groceries</MenuItem>
+              <MenuItem value="utilities">Utilities</MenuItem>
+              <MenuItem value="entertainent">Entertainment</MenuItem>
+            </Select>{" "}
+          </FormControl>
+          <TextField
+            id="outlined-basic"
+            label="Currency"
+            variant="outlined"
+            size="small"
+            gutterBottom
+            fullWidth
+          />
+          <Button
+            type="submit"
+            variant="contained"
+            fullWidth
+            sx={{ backgroundColor: "#183D3D" }}
+          >
+            Submit
+          </Button>
+        </Box>
+      </Paper>
+    </Box>
+  );
 };
 
 export default AddExpenses;
